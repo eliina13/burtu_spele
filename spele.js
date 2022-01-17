673a5297920494449973923302c60fc71b77f4ca
eliina13 <92846329+eliina13@users.noreply.github.com>
2022-01-17 20:49:45 +0200

    Update spele.js

---
+++ spele.js
@@ -0,0 +1,27 @@
+const timeH = document.querySelector ('h4');
+let timeSecond = 5;
+
+timeH.innerHTML = `00:${timeSecond}`;
+
+
+const countDown = setInterval (()=>{
+  timeSecond--;
+  displayTime (timeSecond);
+  if (timeSecond <= 0 || timeSecond < 1) {
+    endTime();
+    clearInterval(countDown);
+  }
+}, 1000)
+
+function displayTime (second) {
+  const min = Math.floor ( second/60);
+  const sec =  Math.floor ( second % 60);
+  timeH.innerHTML = `${min}:${sec}`
+}
+
+function endTime ( ) {
+  timeH.innerHTML = 'Laiks beidzies!';  
+}
+
+
+
