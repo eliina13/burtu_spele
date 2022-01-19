c30420cc605a107297d5fd0b0fa8f2d555ebc9d1
eliina13 <92846329+eliina13@users.noreply.github.com>
2022-01-19 20:25:10 +0200

    Update spele.js

--- spele.js
+++ spele.js
@@ -1,69 +1,27 @@
-window.addEventListener ('load', init);
+const msg = document.querySelector ('.msg');
+const guess = document.querySelector ('input');
+const btn = document.querySelector ('.btn');
+let play = false;
+let sWord= ['skola', 'pavasaris', 'ritenis', 'māja', 'vizbulītes', 'nakts', 'mēness'];
+let newWords = "";
+let randWords = "";
 
-let time = 60;
-let score = 0;
-let isPlaying;
-
-const wordInput = document.querySelector ('wordInput');
-const currentWord = document.querySelector ('currentWord');
-const scoreDisplay = document.querySelector ('score');
-const timeDisplay = document.querySelector ('time');
-const message = document.querySelector ('message');
-const seconds = document.querySelector ('seconds');
-
-const words = [ 'skola', 'laiks', 'mamma'];
-
-//Speles veidošana
-function init () {
-  //ielādē vārdus no apgabala
-  showWord (words);
-  wordInput.addEventListener ('input', startMatch)
-
-  function startMatch (){
-    if (matchWords ()) {
-      isPlaying =true;
-      time = 60;
-      showWord (words);
-      wordInput.value= '';
-      score ++;
-  }
-  if(score === -1) {
-    scoreDisplay.innerHTML = 0;
-  }
-  else{ 
-    scoreDisplay.innerHTML = score;
-  }
-  scoreDisplay.innerHTML = score;
+const creatNewWords = () => {
+  let ranNum = Math.floor(Math.random() * sWord.length);
+  let newTempSwords= sWord [ranNum];
+  console.log(newTempSwords.split(""));
+  return newTempSwords;
 }
 
-function matchWords ( ) {
-  if (wordInput.value === currentWord.innerHTML) {
-    message.innerHTML = 'Pareizi';
-      return true;}
-  else {
-    message,innerHTML = '';
-    return false;
+//const scrambleWords = (arr)=> {}
+  
+
+btn.addEventListener ('click', function () {
+  if (!play){
+    play = true;
+    btn.innerHTML = "Guess";
+    guess.classList.toggle ('hidden');
+    newWords = creatNewWords ();
+   // randWords = scrambleWords( newWords)
   }
-}
-const randIndex = Math.floor (Math.random()* words.length);
-
-// Izvade random vārdiem
-
-currentWord.innerHTML = words [randIndex];
-
-
-//laiks
-setInterval (countDown, 1000);
-setInterval (checkStatus, 50)
-function countDown () {
-  if (time >0) {
-    time--;}
-    else if (time === 0) {
-      isPlaying =false;
-    }
-    timeDisplay.innerHTML = time;
-}
-//Parbauda statusu
-function checkStatus () {
-  if (!isPlaying && time === 0) {message.innerHTML = 'Spēle beigusies!'}
-score = 0;}
+})
